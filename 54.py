bc78e114b4cee299470a09b05c445686531099ce
Maxmeme <42377385+Maxmeme@users.noreply.github.com>
2018-11-19 20:37:12 +0100

    Finished 54

--- 54.py
+++ 54.py
@@ -18,24 +18,70 @@ with open('storage//54_poker.txt','r') as f:
 def value(cards):
     values = Counter(cards[0])
     colors = Counter(cards[1])
-    functions = [
-        royalflush(values, colors),
-        straightflush(values, colors),
-        fourofakind(values, colors),
-        fullhouse(values, colors),
-        flush(values, colors),
-        straight(values, colors),
-        threeofakind(values, colors),
-        twopairs(values, colors),
-        onepair(values, colors),
-        highestcard(values, colors),
-    ]
-    for i in functions:
-        if i:
-            return i
-def royalflush():
+
+    isstraight = sorted(values.keys()) == list(range(min(values.keys()), max(values.keys())+1))
+    samesuit = 5 in colors.values()
+    # flush
+    if samesuit and not isstraight:
+        return 500 + max(values.keys())
+
+    maxcounter = 0
+    for v in values.values():
+        if v > maxcounter:
+            maxcounter = v
+    # High Card, Straight, Straight Flush, Royal Flush
+    if maxcounter == 1:
+        if isstraight:
+            # Straight Flush, Royal Flush
+            if samesuit:
+                # Royal Flush
+                if min(values.keys()) == 8:
+                    return 900
+                # Straight Flush
+                else:
+                    return 800 + max(values.keys())
+            # Straight
+            else:
+                return 400 + max(values.keys())
+        # High Card
+        else:
+            return max(values.keys())
+    # One Pair, Two Pairs
+    elif maxcounter == 2:
+        maxpair = 0
+        numberofpairs = 0
+        for k,v in values.items():
+            if v == 2:
+                numberofpairs += 1
+                if k > maxpair:
+                    maxpair = k
+        # One Pair
+        if numberofpairs == 1:
+            return 100 + maxpair
+        # Two Pairs
+        else:
+            return 200 + maxpair
+        
+    # Three of a kind, Full House
+    elif maxcounter == 3:
+        ispair = False
+        for v in values.values():
+            if v == 2:
+                ispair = True
+
+        # Full House
+        if ispair:
+            return 600 + max([k for k,v in values.items() if v == 3])
+        # Three of a kind
+        else:
+            return 300 + max([k for k,v in values.items() if v == 3])
+
+    # Four of a kind
+    else:
+        return 700 + max([k for k,v in values.items() if v == 4])
+    return 0
     
-count = 1
+count = 0
 for game in games:
     if value(game[0]) > value(game[1]):
         count += 1
