1336282ab922d4f20d15165b94481619b5fcc67a
Maxmeme <42377385+Maxmeme@users.noreply.github.com>
2018-11-27 21:42:34 +0100

    Added solution 65

---
+++ 65.py
@@ -0,0 +1,36 @@
+from math import gcd
+
+def sum_digits(n):
+    s = 0
+    while n:
+        s += n % 10
+        n //= 10
+    return s
+
+def to_fraction(sequence):
+    last = [1, sequence[-1]]
+    sequence.pop(-1)
+    count = 0
+    for i in reversed(sequence):
+        last[0] = i * last[1] + last[0]
+        last[0], last[1] = last[1], last[0]
+        count += 1
+    divisor = 0
+    while divisor != 1:
+        divisor = gcd(last[0], last[1])
+        last = [last[0]//divisor, last[1]//divisor]
+
+    return  [last[1], last[0]]
+
+
+limit = 100
+# there is a pattern in the continued fraction
+# it starts with 1,2,1 and every third number after that gets increased by 2
+sequence = [2, 1]
+for i in range(2, limit):
+    if i % 3 == 2:
+        sequence.append((i//3) * 2 + 2)
+    else:
+        sequence.append(1)
+
+print(sum_digits(to_fraction(sequence)[0]))
