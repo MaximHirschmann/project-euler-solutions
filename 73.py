a10d75d4681ff86a7bdd64fd19d85502b203d4fe
Maxmeme <42377385+Maxmeme@users.noreply.github.com>
2018-12-01 14:48:51 +0100

    Added solution 73

---
+++ 73.py
@@ -0,0 +1,26 @@
+from math import ceil
+from time import time
+from Utils import save_time
+
+start = time()
+
+def frac(d):
+    sieve = [0] * (d + 1)
+    for denominator in range(5, d + 1):
+        if denominator % 3 == 0:
+            lower_limit = denominator // 3 + 1
+        else:
+            lower_limit = ceil(denominator / 3)
+        if denominator % 2 == 0:
+            upper_limit = denominator // 2 - 1
+        else:
+            upper_limit = denominator // 2
+        # add range of numbers which are > 1/3 and < 1/2
+        sieve[denominator] += upper_limit - lower_limit + 1
+        # substract multiples of already found fractions
+        for j in range(2 * denominator, d + 1, denominator):
+            sieve[j] -= sieve[denominator]
+    return sum(sieve)
+
+print(frac(12000))
+save_time(73, time()-start)
