77eecf07fa5b91b5834c370cb63d03ee2c40f7e5
Maxmeme <42377385+Maxmeme@users.noreply.github.com>
2018-12-01 15:14:28 +0100

    Added solution 74

---
+++ 74.py
@@ -0,0 +1,27 @@
+from math import factorial
+
+def sum_factorial_digits(n):
+    return sum(factorials[i] for i in str(n))
+
+factorials = {str(i): factorial(i) for i in range(10)}
+lengths = {}
+count = 0
+for i in range(1,1000000):
+    res = i
+    chain = [i]
+    length = 1
+    while True:
+        res = sum_factorial_digits(res)
+        if res in lengths:
+            length += lengths[res]
+            break
+        else:
+            if res in chain:
+                break
+            chain.append(res)
+            length += 1
+    for index, num in enumerate(chain):
+        lengths[num] = length - index
+    if length == 60:
+        count += 1
+print(count)
